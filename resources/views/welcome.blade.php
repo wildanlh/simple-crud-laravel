ab6e8c25d9e1d73d783544ce810587145d20b790
Wildan Luqmanul Hakim <wildanluqmanul@gmail.com>
2023-06-20 16:54:55 +0700

    Update mobile screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,16 +10,19 @@
 
 <body class="selection:bg-[#FFFFA6] selection:text-black">
     <div class="h-screen flex justify-center items-center">
-        <div class="shadow-lg p-10 rounded">
-            <h2 class="text-5xl font-bold">Halo! &#128075;</h2>
-            <h1 class="text-6xl font-bold text-[#007AFF]">
-                Selamat Datang
-            </h1>
-            <div class="flex justify-center gap-5 mt-10">
-                <a href="/v1/create" class="text-[#007AFF] font-medium py-2 px-4 relative border border-[#007AFF] rounded bg-transparent transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#007AFF] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100">Tambah Produk</a>
-                <a href="/v1" class="text-[#007AFF] font-medium py-2 px-4 relative border border-[#007AFF] rounded bg-transparent transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#007AFF] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100">Lihat Tabel</a>
+        <div class="container mx-auto">
+            <div class="md:shadow-lg p-10 rounded">
+                <h2 class="text-5xl font-bold">Halo! &#128075;</h2>
+                <h1 class="text-6xl font-bold text-[#007AFF]">
+                    Selamat Datang
+                </h1>
+                <div class="flex justify-center gap-5 mt-10">
+                    <a href="/v1/create" class="text-[#007AFF] font-medium py-2 px-4 relative border border-[#007AFF] rounded bg-transparent transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#007AFF] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100">Tambah Produk</a>
+                    <a href="/v1" class="text-[#007AFF] font-medium py-2 px-4 relative border border-[#007AFF] rounded bg-transparent transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#007AFF] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100">Lihat Tabel</a>
+                </div>
             </div>
         </div>
+
     </div>
 
 </body>
